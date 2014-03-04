c7d2902475561fc4e9794df7ae70623fbbd0f6fe
David Irvine <irvined@gmail.com>
2014-03-03 20:09:53 -0800

    Fixed get_token

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
 		name='django-openlavaweb',
